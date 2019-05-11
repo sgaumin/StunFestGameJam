3e4e9aa48a4d553753fd86044d70ad05dac17bfd
TsukinoNeko <morgane.carry@gmail.com>
2019-05-11 23:50:47 +0200

    Added switch of message change order

--- Assets/Scripts/Objects/MessageChanger.cs
+++ Assets/Scripts/Objects/MessageChanger.cs
@@ -7,6 +7,9 @@ public class MessageChanger : MonoBehaviour
 
     public MessageChangerType messageChangerType;
 
+    public static bool MessageChangerColorClockwiseRotation = true;
+    public static bool MessageChangerShapeClockwiseRotation = true;
+
     [SerializeField] private Plug plugIn;
 
     public void ChangeMessage(Message message)
@@ -14,38 +17,77 @@ public class MessageChanger : MonoBehaviour
         switch (messageChangerType)
         {
             case MessageChangerType.Shape:
-                switch (message.messageShape)
+                if(MessageChangerShapeClockwiseRotation)
                 {
-                    case MessageShapes.Cube:
-                        message.messageShape = MessageShapes.Circle;
-                        break;
-                    case MessageShapes.Circle:
-                        message.messageShape = MessageShapes.Triangle;
-                        break;
-                    case MessageShapes.Triangle:
-                        message.messageShape = MessageShapes.Cube;
-                        break;
-                    default:
-                        throw new ArgumentOutOfRangeException();
+                    switch (message.messageShape)
+                    {
+                        case MessageShapes.Cube:
+                            message.messageShape = MessageShapes.Circle;
+                            break;
+                        case MessageShapes.Circle:
+                            message.messageShape = MessageShapes.Triangle;
+                            break;
+                        case MessageShapes.Triangle:
+                            message.messageShape = MessageShapes.Cube;
+                            break;
+                        default:
+                            throw new ArgumentOutOfRangeException();
+                    }
                 }
-
+                else
+                {
+                    switch (message.messageShape)
+                    {
+                        case MessageShapes.Cube:
+                            message.messageShape = MessageShapes.Triangle;
+                            break;
+                        case MessageShapes.Circle:
+                            message.messageShape = MessageShapes.Cube;
+                            break;
+                        case MessageShapes.Triangle:
+                            message.messageShape = MessageShapes.Circle;
+                            break;
+                        default:
+                            throw new ArgumentOutOfRangeException();
+                    }
+                }
                 break;
             case MessageChangerType.Color:
-                switch (message.messageColor)
+                if (MessageChangerColorClockwiseRotation)
                 {
-                    case MessageColors.Red:
-                        message.messageColor = MessageColors.Green;
-                        break;
-                    case MessageColors.Green:
-                        message.messageColor = MessageColors.Blue;
-                        break;
-                    case MessageColors.Blue:
-                        message.messageColor = MessageColors.Red;
-                        break;
-                    default:
-                        throw new ArgumentOutOfRangeException();
-                }
+                    switch (message.messageColor)
+                    {
+                        case MessageColors.Red:
+                            message.messageColor = MessageColors.Green;
+                            break;
+                        case MessageColors.Green:
+                            message.messageColor = MessageColors.Blue;
+                            break;
+                        case MessageColors.Blue:
+                            message.messageColor = MessageColors.Red;
+                            break;
+                        default:
+                            throw new ArgumentOutOfRangeException();
+                    }
 
+                }
+                else
+                {
+                    switch (message.messageColor)
+                    {
+                        case MessageColors.Red:
+                            message.messageColor = MessageColors.Blue;
+                            break;
+                        case MessageColors.Green:
+                            message.messageColor = MessageColors.Red;
+                            break;
+                        case MessageColors.Blue:
+                            message.messageColor = MessageColors.Green;
+                            break;
+                        default:
+                            throw new ArgumentOutOfRangeException();
+                    }
+                }
                 break;
             default:
                 throw new ArgumentOutOfRangeException();
---
+++ Assets/Scripts/Objects/Switch.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch : MonoBehaviour
+{
+
+    public MessageChangerType messageChangerType;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        SwitchChangerOrder();
+    }
+
+    void SwitchChangerOrder()
+    {
+        switch (messageChangerType)
+        {
+            case MessageChangerType.Color:
+                MessageChanger.MessageChangerColorClockwiseRotation = !MessageChanger.MessageChangerColorClockwiseRotation;
+                break;
+            case MessageChangerType.Shape:
+                MessageChanger.MessageChangerShapeClockwiseRotation = !MessageChanger.MessageChangerShapeClockwiseRotation;
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+    }
+}
