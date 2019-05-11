bdfe0eb9d0590b0c58e7a6b99a43867e7774921c
TsukinoNeko <morgane.carry@gmail.com>
2019-05-11 14:16:19 +0200

    Added characters screens

---
+++ Assets/Scripts/Objects/ScreenController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScreenController : MonoBehaviour
+{
+    public float timerSpeed = 1F;
+    float timer = 1F;
+    float originalSize;
+    float totalTime = 0F;
+
+    public Image timerFill;
+    public GameObject mire;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        originalSize = timerFill.rectTransform.rect.height;
+        mire.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        totalTime += Time.deltaTime;
+        if(timerFill.rectTransform.rect.height > 0)
+        {
+            // resize the timer fill
+            float newSize = originalSize * (1 - totalTime * timerSpeed);
+            timerFill.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, newSize);
+        } else
+        {
+            Debug.Log("timer over");
+            // show the mire when time is over
+            mire.SetActive(true);
+        }
+
+    }
+}
