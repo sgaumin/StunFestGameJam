f80cef9c7ded124fbb975dce5eb220de5de7b49e
TsukinoNeko <morgane.carry@gmail.com>
2019-05-12 16:10:02 +0200

    changement couleur + débranchement + facile + correction bug bonhommes

---
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+
+    public static AudioManager Instance;
+
+    private AudioSource _audioSource;
+
+    [SerializeField] private AudioClip[] plugSounds;
+    [SerializeField] private AudioClip unplugSound;
+
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else if (Instance != null)
+            Destroy(gameObject);
+    }
+
+    private void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+    public void PlayUnplugSound()
+    {
+        _audioSource.clip = unplugSound;
+        _audioSource.Play();
+    }
+
+
+    public void PlayPlugSound()
+    {
+        _audioSource.clip = plugSounds[Random.Range(0, plugSounds.Length)];
+        _audioSource.Play();
+    }
+
+
+}
